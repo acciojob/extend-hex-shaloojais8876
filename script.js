670f6326f35f70c7efceaefbc178170c47c36b36
shaloojais8876 <104994730+shaloojais8876@users.noreply.github.com>
2024-08-03 21:52:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	//shortHex = "aB0"
+	let i = shortHex.length - 3, isCapital = false;
+	let output = "#";
+	while(i < shortHex.length){
+		let asciiCode = shortHex[i].charCodeAt(0);
+		if(asciiCode >= 65 && asciiCode <= 90){
+			isCapital = true;
+		}
+		//output = "#" += "a" = "a" => "#aa"
+		output += shortHex[i] + shortHex[i];
+		i++;
+	}
+	return isCapital ? output.toUpperCase() : output;
 };
 
 // Do not change the code below.
